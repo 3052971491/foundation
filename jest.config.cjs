4de799397e2934dcd2fc75b7334c9e739f7bdb03
yuanjunjie <junjie.yuan@gct-china.com>
2022-09-27 11:42:43 +0800

    chore: 新增jest测试依赖

---
+++ jest.config.cjs
@@ -0,0 +1,34 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+
+module.exports = {
+  preset: 'ts-jest',
+  roots: ['<rootDir>/tests/'],
+  clearMocks: true,
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['ts', 'vue', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePaths: ['<rootDir>/src', '<rootDir>/node_modules'],
+  testMatch: [
+    '**/tests/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[tj]s?(x)',
+    '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
+  ],
+  testPathIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+  // A map from regular expressions to module names that allow to stub out resources with a single module
+  moduleNameMapper: {
+    '\\.(vs|fs|vert|frag|glsl|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/$1',
+    '\\.(sass|s?css|less)$': '<rootDir>/src/$1',
+    '\\?worker$': '<rootDir>/src/$1',
+    '^/@/(.*)$': '<rootDir>/src/$1',
+  },
+  testEnvironment: 'jsdom',
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.{ts,vue. tsx,jsx,js}'],
+  coveragePathIgnorePatterns: ['^.+\\.d\\.ts$'],
+};
---
+++ tests/test.spec.ts
@@ -0,0 +1,5 @@
+describe('测试模块', () => {
+  test('这是一条测试信息', async () => {
+    expect('jest').toBe('jest');
+  });
+});
